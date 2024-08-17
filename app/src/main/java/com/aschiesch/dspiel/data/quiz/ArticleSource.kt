9ea435e15f2b147f61bcacdfb65fc6afe963f3b3
AshishPro04 <ashishcjoseph@outlook.com>
2024-08-17 21:34:25 +0530

    Added negative article support

--- app/src/main/java/com/aschiesch/dspiel/data/quiz/ArticleSource.kt
+++ app/src/main/java/com/aschiesch/dspiel/data/quiz/ArticleSource.kt
@@ -3,7 +3,10 @@ package com.aschiesch.dspiel.data.quiz
 import com.aschiesch.dspiel.R
 
 object ArticleSource {
-    val negativeArticles: List<ArticleResource> = listOf()
+    val negativeArticleOptions= listOf(
+        R.string.negative_article_nominative_option_1,
+        R.string.negative_article_nominative_option_2,
+    )
     private val definiteArticleOptions = listOf(
         R.string.definite_article_nominative_option_1,
         R.string.definite_article_nominative_option_2,
@@ -315,6 +318,159 @@ object ArticleSource {
             question = R.string.indefinite_article_nominative_question_30,
             answer = R.string.indefinite_article_nominative_option_2,
             options = indefinitiveArticleOptions
+        )
+    )
+
+    val negativeArticles = listOf(
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_1,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_2,
+            answer = R.string.negative_article_nominative_option_2,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_3,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_4,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_5,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_6,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_7,
+            answer = R.string.negative_article_nominative_option_2,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_8,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_9,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_10,
+            answer = R.string.negative_article_nominative_option_2,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_11,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_12,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_13,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_14,
+            answer = R.string.negative_article_nominative_option_2,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_15,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_16,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_17,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_18,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_19,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_20,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_21,
+            answer = R.string.negative_article_nominative_option_2,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_22,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_23,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_24,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_25,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_26,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_27,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_28,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_29,
+            answer = R.string.negative_article_nominative_option_1,
+            options = negativeArticleOptions
+        ),
+        ArticleResource(
+            question = R.string.negative_article_nominative_question_30,
+            answer = R.string.negative_article_nominative_option_2,
+            options = negativeArticleOptions
         )
     )
 }
